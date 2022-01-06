88b3c72ce0f06e780b0ab50b97276b641c9fba33
Osagumi <38025785+Osagumi@users.noreply.github.com>
2022-01-05 22:29:45 -0300

    Add files via upload

---
+++ el juegardo/el juegardo/juego.py
@@ -0,0 +1,16 @@
+import pygame
+import os
+
+
+# Game Setup
+FPS = 60
+fpsClock = pygame.time.Clock()
+WINDOW_WIDTH = 900
+WINDOW_HEIGHT = 600
+
+WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('sims pero fome')
+
+
+    
+
